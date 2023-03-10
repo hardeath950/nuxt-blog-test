fb775619ac810931037bc433450bd3de114c86c4
Raul Domingues <raaaaul@live.com>
2023-03-10 18:48:03 -0300

    fix post type

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,18 @@
 export { }
 
 declare global {
-  interface Post {
+  export interface Post {
     slug: string
+    status: string
+    sort: any
+    user_created: string
+    date_created: string
+    user_updated?: string
+    date_updated?: string
     title: string
     content: string
+    summary: string
+    image: string
   }
+
 }
